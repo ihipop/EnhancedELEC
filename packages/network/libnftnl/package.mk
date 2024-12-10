3f4f56eb4dbf4765595fda12c027900cc031f733
ihipop <ihipop@gmail.com>
2024-12-10 17:57:04 +0800

    [package]  upgrade nftables and it's dependence to v1.1.1; fix the dependency of gmp

--- packages/network/libnftnl/package.mk
+++ packages/network/libnftnl/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnftnl"
-PKG_VERSION="1.2.3"
-PKG_SHA256="e916ea9b79f9518560b9a187251a7c042442a9ecbce7f36be7908888605d0255"
+PKG_VERSION="1.2.8"
+PKG_SHA256="37fea5d6b5c9b08de7920d298de3cdc942e7ae64b1a3e8b880b2d390ae67ad95"
 PKG_LICENSE="GPL"
 PKG_SITE="http://netfilter.org/projects/libnftnl"
-PKG_URL="http://netfilter.org/projects/libnftnl/files/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="http://netfilter.org/projects/libnftnl/files/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libmnl"
 PKG_LONGDESC="A userspace library providing a low-level netlink programming interface (API) to the in-kernel nf_tables subsystem."
 
--- packages/network/nftables/package.mk
+++ packages/network/nftables/package.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 PKG_NAME="nftables"
-PKG_VERSION="1.0.5"
-PKG_SHA256="8d1b4b18393af43698d10baa25d2b9b6397969beecac7816c35dd0714e4de50a"
+PKG_VERSION="1.1.1"
+PKG_SHA256="6358830f3a64f31e39b0ad421d7dadcd240b72343ded48d8ef13b8faf204865a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://netfilter.org/projects/nftables"
-PKG_URL="http://netfilter.org/projects/${PKG_NAME}/files/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain libnftnl libedit"
+PKG_URL="http://netfilter.org/projects/${PKG_NAME}/files/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain libnftnl libedit gmp"
 PKG_LONGDESC="nftables is the modern Linux kernel packet classification framework"
