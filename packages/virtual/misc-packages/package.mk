f86ae062b4f0847efa7e539fab66510b74216d3e
ihipop <ihipop@gmail.com>
2025-03-30 20:03:35 +0800

    [package] add entware package and enable it by default with RK3399
    This reverts commit 3f74deab9134a3ba034b97a58fd0411acb828188.

--- packages/virtual/misc-packages/package.mk
+++ packages/virtual/misc-packages/package.mk
@@ -9,3 +9,10 @@ PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain ${ADDITIONAL_PACKAGES}"
 PKG_SECTION="virtual"
 PKG_LONGDESC="misc-packages: Metapackage for miscellaneous packages"
+
+# Entware support
+if [ "${ENTWARE_SUPPORT}" = "yes" ]; then
+  mkdir -p ${INSTALL}
+  ln -sf /storage/.opt ${INSTALL}/opt
+  PKG_DEPENDS_TARGET+=" entware"
+fi
