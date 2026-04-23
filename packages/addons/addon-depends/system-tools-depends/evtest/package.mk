5076d07fd677b74e743bd55854f58b32779b18d0
ihipop <ihipop@gmail.com>
2026-04-23 18:59:02 +0800

    feat(package): upd evtest

--- packages/addons/addon-depends/system-tools-depends/evtest/package.mk
+++ packages/addons/addon-depends/system-tools-depends/evtest/package.mk
@@ -3,10 +3,10 @@
 
 PKG_NAME="evtest"
 PKG_VERSION="1.35"
-PKG_SHA256="a224abeb783561193660999b0c986216d691cb271b5ec3af88c6bb37b55ff51c"
+PKG_SHA256="06dfe6b9760b78f3f73aca2120cbcb79339b33e59d5c79a49b4bd5d34844b054"
 PKG_LICENSE="GPL"
 PKG_SITE="http://cgit.freedesktop.org/evtest/"
-PKG_URL="https://repo.or.cz/evtest.git/snapshot/evtest-${PKG_VERSION}.tar.gz"
+PKG_URL="https://gitlab.freedesktop.org/libevdev/evtest/-/archive/evtest-${PKG_VERSION}/evtest-evtest-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libxml2"
 PKG_LONGDESC="A simple tool for input event debugging."
 PKG_TOOLCHAIN="autotools"
